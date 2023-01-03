dde1177a1afc86748d22b53c2e8b5a8028df421d
weddleben <stomachworm88@gmail.com>
2023-01-03 15:58:20 -0600

    adding today_is_weekday function
    importing the today_is_weekday function and moving the rest of the script under the 'main' function. we only want to run this script on weekdays, otherwise a notification is triggered, since we only have episodes for weekdays.

--- talklib_examples/SoundBeat.py
+++ talklib_examples/SoundBeat.py
@@ -1,10 +1,13 @@
 from talklib.show import TLShow
+from talklib.utils import today_is_weekday
 import glob
 from datetime import datetime
 
 
 def matchName():
     '''TODO explain this'''
+    folder = r'D:\Production\shows\SoundBeat'
+
     dict = {
     'Mon': 'SGMT01',
     'Tue': 'SGMT02',
@@ -22,16 +25,19 @@ def matchName():
             file_to_use = file
 
     return  file_to_use
-    
-folder = r'D:\Production\shows\SoundBeat'
-file_to_use = matchName()
 
-SB = TLShow()
-SB.show = 'Sound Beat'
-SB.show_filename = 'Tenn-SoundBeat'
-SB.is_local = True
-SB.local_file = file_to_use
-SB.remove_source = True
-SB.check_if_above = 1.51
-SB.check_if_below = 1.49
-SB.run()
+def main():
+    file_to_use = matchName()
+
+    SB = TLShow()
+    SB.show = 'Sound Beat'
+    SB.show_filename = 'Tenn-SoundBeat'
+    SB.is_local = True
+    SB.local_file = file_to_use
+    SB.remove_source = True
+    SB.check_if_above = 1.51
+    SB.check_if_below = 1.49
+    SB.run()
+
+if today_is_weekday():
+    main()
