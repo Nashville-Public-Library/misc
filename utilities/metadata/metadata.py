f3a474b25312628ab7998b8f2e8385dcb06e32a9
weddleben <stomachworm88@gmail.com>
2023-01-05 16:48:59 -0600

    use IP instead of environment variable
    swapping in the actual IP addresses instead of local environment variables. it feels simpler this way. unless we need the either the Telos or BrightSign units in other scripts, then we can change it back.

--- utilities/metadata/metadata.py
+++ utilities/metadata/metadata.py
@@ -13,14 +13,16 @@ object_for_notifications = TLShow(show=f'Metadata')
 error_message = f'There was a problem sending metadata for {title}'
 success_message = f' "{title}" sent to encoder'
 
+# send message to Telos unit
 try:
-    subprocess.run(f'echo t={title} | ncat %Telos%', shell=True, timeout=5)
+    subprocess.run(f'echo t={title} | ncat 10.28.30.129 9000', shell=True, timeout=5)
     object_for_notifications.syslog(message=success_message)
 except:
     object_for_notifications.notify(message=error_message, subject='Error')
 
+# send message to BrightSign unit
 try:
-    subprocess.run(f'echo {title} | ncat -u %BrightSign%', shell=True, timeout=5)
+    subprocess.run(f'echo {title} | ncat -u 10.28.30.212 5000', shell=True, timeout=5)
     object_for_notifications.syslog(message=success_message)
 except:
     object_for_notifications.notify(message=error_message, subject='Error')
