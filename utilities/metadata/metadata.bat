064bf00ab36f333d18e6f40c9f942e29fbe507fb
weddleben <stomachworm88@gmail.com>
2023-01-05 16:37:39 -0600

    update example batch file
    adding comments up top, the CD function, and the Python call.

--- utilities/metadata/metadata.bat
+++ utilities/metadata/metadata.bat
@@ -1,1 +1,12 @@
-py delete.py --title "Tennessean - Front Page, Page 2"
+:: This will launch the python script.
+:: WireReady cannot run .py files, so a batch file is needed to do it.
+:: WireReady will run the batch file which runs the python file.
+
+:: turn echo off
+@echo off
+
+:: change to current directory (directory of batch file)
+CD /d "%~dp0"
+
+:: run python script with command line argument
+py %CD%\metadata.py --title "Name of Some Program"
