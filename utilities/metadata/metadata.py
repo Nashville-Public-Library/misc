309054a807ce1ade854825aac74d99ea8c062e66
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-02-21 16:28:24 -0600

    NO MORE NCAT!!!!!!!!!!!!!!!!
    finally, at long last, after a month+ of trying to get this to work......it seems we have a working, pure python way of sending Now Playing metadata to our Telos unit (which forwards it to Icecast) AND the BrightSign unit.
    
    it uses Python's socket library, part of the standard library. I had tried experimenting with this before but could not get it working. it turns out the problem had to do with the metadata filter we were using on the Telos unit. now we switched to this more explicit filter (XML Parser Sample), which gives easier formatting that won't confuse Python or the Telos unit.
    
    separately, we're using UDP to send to BrightSign (only accepts UDP, weirdly), and that doesn't require filtering/parsing.
    
    YAAAAAAAAYYYYY!!!!!! :)

--- utilities/metadata/metadata.py
+++ utilities/metadata/metadata.py
@@ -2,14 +2,11 @@
 Send metadata to Icecast and wherever else you need it.
 I haven't yet figured out how to get this to work with Python's socket library,
 so we're using ncat.
-
 Just call up this script from the command line or a bash/batch script and pass
 in the command line argument.
 '''
-
-import subprocess
 import argparse
-import time
+import socket
 
 from talklib.show import TLShow
 
@@ -21,30 +18,30 @@ title = args.title
 send_syslog = TLShow(show=f'Metadata')
 
 error_message = f'There was a problem sending metadata for "{title}".'
-success_message = f' "{title}" sent to encoder'
 
-# send message to Telos unit
-
-process = subprocess.Popen(f'echo t={title} | ncat 10.28.30.129 9000', shell=True)
-send_syslog.syslog(message=success_message)
-
+# Telos
 try:
-    outs, errs = process.communicate(timeout=10)
+    host = '10.28.30.129'
+    port = 9000
+
+    to_send = f'<song title="{title}" url="..."></song>'
+    to_send = to_send.encode()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    sock.sendall(to_send)
+    sock.close()
+    send_syslog.syslog(message=f'{title} sent to Telos')
 except:
-    process.kill()
-    process.communicate()
-    send_syslog.notify(message=error_message, subject='Error')
+    send_syslog.syslog(message=error_message)
 
-# BrightSign
 
-process2 = subprocess.Popen(f'echo {title} | ncat -u 10.28.30.212 5000', shell=True,)
-send_syslog.syslog(message=success_message)
-
-time.sleep(2)
-
+# BrightSign 
 try:
-    outs, errs = process2.communicate(timeout=10)
+    to_send_UDP = title.encode()
+    serverAddressPort   = ("10.28.30.212", 5000)
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPClientSocket.sendto(to_send_UDP, serverAddressPort)
+    send_syslog.syslog(message=f'{title} sent to BrightSign')
 except:
-    process2.kill()
-    process2.communicate()
-    send_syslog.notify(message=error_message, subject='Error')
+    send_syslog.syslog(message=error_message)
