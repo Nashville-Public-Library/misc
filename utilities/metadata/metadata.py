af6d59107247740a0e123cb3a1c8da5063e8e01d
weddleben <stomachworm88@gmail.com>
2023-03-22 15:10:16 -0500

    move into functions, add syslog and notifications

--- utilities/metadata/metadata.py
+++ utilities/metadata/metadata.py
@@ -16,36 +16,46 @@ import requests
 
 from talklib.show import TLShow
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--title', type=str, required=True)
-args = parser.parse_args()
-title = args.title
+meta = TLShow(show=f'Metadata')
 
-send_syslog = TLShow(show=f'Metadata')
+def get_title():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--title', type=str, required=True)
+    args = parser.parse_args()
+    title = args.title
+    return title
 
-error_message = f'There was a problem sending metadata for "{title}".'
+def send_to_icecast(title):
+    meta.syslog(message=f'attempting to send "{title}" to Icecast')
+    user = os.environ['icecast_user']
+    password = os.environ['icecast_pass']
+    url = f'https://npl.streamguys1.com:80/admin/metadata?mount=/live&mode=updinfo&song={title}'
+    send = requests.get(url, auth = (user, password))
+    if send.status_code != 200:
+        meta.notify(
+            subject='Error', 
+            message=f'There was a problem sending metadata to Icecast. The response code was: {send.status_code}'
+            )
+    else:
+        meta.syslog(message=f'"{title}" sent to Icecast')
 
-print(f'Metadata: sending "{title}" ...')
 
-# Telos
+def send_to_BrightSign(title):
+    try:
+        meta.syslog(message=f'attempting to send "{title}" to BrightSign"')
+        to_send_UDP = title.encode()
+        serverAddressPort   = ("10.28.30.212", 5000)
+        UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        UDPClientSocket.sendto(to_send_UDP, serverAddressPort)
+        meta.syslog(message=f'"{title}" sent to BrightSign')
+    except:
+        meta.notify(subject='Error', message=f'There was a problem sending the title to the BrightSign unit')
 
-user = os.environ['icecast_user']
-password = os.environ['icecast_pass']
 
-url = f'http://npl.streamguys1.com:80/admin/metadata?mount=/live&mode=updinfo&song={title}'
+def main():
+    title = get_title()
+    print(f'...sending "{title}..."')
+    send_to_icecast(title=title)
+    send_to_BrightSign(title=title)
 
-send = requests.get(url, auth = (user, password))
-
-print(send.status_code)
-
-
-# BrightSign 
-
-# try:
-#     to_send_UDP = title.encode()
-#     serverAddressPort   = ("10.28.30.212", 5000)
-#     UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-#     UDPClientSocket.sendto(to_send_UDP, serverAddressPort)
-#     send_syslog.syslog(message=f'{title} sent to BrightSign')
-# except:
-#     send_syslog.syslog(message=error_message)
+main()
