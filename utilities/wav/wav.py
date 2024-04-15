f9cf8f30de883105a6c90597fde39715c0ba8724
weddleben <stomachworm88@gmail.com>
2024-04-15 15:50:55 -0500

    list files to be converted
    - show a list of the files to be converted
    - show which file is actively being converted

--- utilities/wav/wav.py
+++ utilities/wav/wav.py
@@ -21,7 +21,7 @@ yes = ['yes', 'y']
 def convert(file):
     '''convert file with ffmpeg, etc. TODO, add better explanation'''
     subprocess.run('cls', shell=True)
-    print('Just a moment...')
+    print(f'Converting {file}...')
     subprocess.run(f'ffmpeg -hide_banner -loglevel quiet -i "{file}" -ac 1 -ar 44100 output.wav', shell=True)
     os.remove(file)
     os.rename("output.wav", file)
@@ -32,16 +32,10 @@ def convert(file):
 def check_exists():
     '''check whether there are any audio files in current directory'''
     if len(file_types) >= 1:
-        pass
+        return file_types
     else:
-        subprocess.run('cls', shell=True)
-        print(f"I can't find any {supported_formats} files in {cd}. Sorry about that.\n\n\
-Do you want to try again?\n")
-        answer = input('Y/N ')
-        if answer in yes:
-            prompt()
-        else:
-            quit()
+        False
+
 def quit():
     '''print message and exit script'''
     subprocess.run('cls', shell=True)
@@ -55,17 +49,27 @@ def prompt():
     subprocess.run('cls', shell=True)
     print('--------------------CONVERT--------------------')
     print()
-    print(f'This will convert the audio files in {cd} to WAV files')
-    print()
-    print('Are you sure you want to convert these files? It cannot be undone.')
-    print()
+    if not check_exists():
+        subprocess.run('cls', shell=True)
+        print(f"--------------------CONVERT--------------------\n\n\
+I can't find any {supported_formats} files in {cd}. Sorry about that.\n\n\
+Do you want to try again?\n")
+        answer = input('Y/N ')
+        if answer in yes:
+            prompt()
+        else:
+            quit()
+
+    print('The following file(s) will be converted: \n')
+    for file in check_exists():
+        print(file)
+    print('\nAre you sure you want to convert these files? It cannot be undone.\n')
+
     answer = input('Y/N ')
     if answer.lower() in yes:
-        check_exists()
         for f in file_types:
             convert(file=f)
-        print('Your files are converted. Enjoy')
-        print()
+        print('Your files are converted. Enjoy\n')
         input('(press enter to quit)')
     else:
         subprocess.run('cls', shell=True)
